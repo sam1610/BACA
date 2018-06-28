a247082a1fdd898d34981a7b50cfabca28b4b606
sam1610 <samir.amri@gmail.com>
2018-06-28 12:33:50 +0300

    home page setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,18 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DataProvider   }  from "../../providers/data/data";
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit{
- events:any;
+ Events:any;
   constructor(public navCtrl: NavController, private rest:DataProvider) {
   }
 
   ngOnInit(){
     this.rest.getData().subscribe(
-        data => this.events=data
-    );
+      data => {
+        this.Events=data;
+        console.log(this.Events);
+}    )
   }
 }
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,15 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
+
 
 
 @Injectable()
 export class DataProvider {
- ApiUrl="../../assets/data/data.json";
+public    ApiURL="./assets/Data/events.json";
 
   constructor(public http: HttpClient) {
-     
+    console.log('Hello DataProvider Provider');
   }
 getData(){
- return  this.http.get(this.ApiUrl);
+return this.http.get(this.ApiURL).pipe(map(
+  data=> data["Events"]
+))
+
+
 }
 }
